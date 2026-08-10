daf3814d77de7a5cec91bcfe8f52e3561ebf40e2
Levi <levilevi@Levis-MacBook-Air.local>
2026-08-10 07:01:01 -0700

    added the command line interface layout for implementation

---
+++ src/cli/cli.py
@@ -0,0 +1,18 @@
+
+class CLI():
+
+    def __init__(self, command_queue):
+        sefl.command_queue = command_queue
+
+    def add():
+        pass
+
+    def remove():
+        pass
+
+    def bypass():
+        pass
+
+    def edit():
+        pass
+        
