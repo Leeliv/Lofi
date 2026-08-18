81179010c96872d2f5849b8c740a6237fb5b7e56
Levi <levilevi@mac.home>
2026-08-18 08:43:29 -0700

    fixed low pass filter buzing sound by filtering one frame at a time insted of chunk by chunk

--- src/processing/lowpassfilter.py
+++ src/processing/lowpassfilter.py
@@ -6,22 +6,30 @@ class LpwPassFilter(Effect):
 
     def __init__(self, smooth_factor, algorithm):
         self.algorithm = algorithm
-        self.smooth_factor = smooth_factor
+        self.parameters = {
+            "smooth_factor": smooth_factor
+        }
 
     def process(self, audio):
         # print("CALLING: low pass filter")
-        return self.algorithm.process(audio, self.smooth_factor)
+        return self.algorithm.process(audio, **self.parameters)
 
 class lpf():
 
     def __init__(self):
-        self.prev_audio = np.full((1024,2),9)
+        self.prev_audio = np.zeros(2, dtype = np.float32)
 
     def process(self, audio, smooth_factor):
         #y[i] = alpha * x[i] + (1 - alpha) * y[i-1]
-        if (self.prev_audio[1] == 9).any():
-            
-            self.prev_audio = audio
-            return audio
+        output = np.empty_like(audio, dtype=np.float32)
 
-        return smooth_factor * audio + (1 - smooth_factor) * self.prev_audio
+        for i, frame in enumerate(audio):
+            output[i] = (smooth_factor * frame + (1 - smooth_factor) * self.prev_audio)
+            self.prev_audio = output[i]
+
+        # if (self.prev_audio[1] == 9).any():
+            
+        #     self.prev_audio = audio
+        #     return audio
+
+        return output
